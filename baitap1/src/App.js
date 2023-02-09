d7f7bd321ca3b12d7a03b84926ccd30b76dc272f
Sang121 <20010797@st.phenikaa-uni.edu.vn>
2023-02-09 17:17:19 +0700

    Bài tập 1

---
+++ baitap1/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css'
+import styles from "./styles.module.css"
+function App() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidebar}> Side bar</div>
+      <div className={styles.content}>Content</div>
+    </div>
+    )
+
+}
+
+export default App;
